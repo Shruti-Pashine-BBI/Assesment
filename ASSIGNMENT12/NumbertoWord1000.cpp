d22e7d34ca80d6e6dc1e683bbfd4e294ead9bf1a
Shruti-Pashine-BBI <141905762+Shruti-Pashine-BBI@users.noreply.github.com>
2023-08-21 10:40:51 +0530

    Update NumbertoWord1000.cpp

--- ASSIGNMENT12/NumbertoWord1000.cpp
+++ ASSIGNMENT12/NumbertoWord1000.cpp
@@ -1,4 +1,6 @@
-#include <iostream>
+ 
+ 
+ #include <iostream>
 #include <string>
 using namespace std;
 string one_nineteen[] = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten","Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
@@ -6,22 +8,20 @@ string twenty_hundred[] = {"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty"
 string thousand[] = {"", "Thousand","Million","Billion"};
 
 string convertLessThanThousand(int num) {
-    if (num ==0) {
+    if (num == 0) {
         return "";
-    } 
-    else if (num < 20) {
+    } else if (num < 20) {
         return one_nineteen[num] + " ";
     } else if (num < 100) {
         return twenty_hundred[num / 10] + " " + convertLessThanThousand(num % 10);
-    }
-    else if (num < 1000) {
-        return twenty_hundred[num / 100] + " Thousand " + convertLessThanThousand(num % 100);
-    }
-     else if (num < 1000) {
-        return twenty_hundred[num / 100] + " Thousand " + convertLessThanThousand(num % 100);
-    }
-    else {
+    } else if(num<1000) {
         return one_nineteen[num / 100] + " Hundred " + convertLessThanThousand(num % 100);
+    }
+    // else if(num<10000){
+    //     return one_nineteen[num / 1000] + " Thousand " + convertLessThanThousand(num % 1000);
+    // }
+    else{
+        return one_nineteen[num / 1000] + " Thousand " + convertLessThanThousand(num % 1000);
     }
 }
 
@@ -29,18 +29,19 @@ string convertLessThanThousand(int num) {
 
 
 int main() {
-    int num;
-    cout<<"Enter the Number:";
-    cin>>num;
-    string words = convertLessThanThousand(num);
-    cout << "\nOutput: " << words <<endl;
-    
-    // for(int i=0;i<=1000;i+=3){
-        
-    //     string words = convertLessThanThousand(i);
+    // int num;
+    // cout<<"Enter the Number:";
+    // cin>>num;
+    // string words = convertLessThanThousand(num);
     // cout << "\nOutput: " << words <<endl;
-    // }
+    
+    for(int i=0;i<=10000;i+=4){
+        
+        string words = convertLessThanThousand(i);
+    cout << "\nOutput: " << words <<endl;
+    }
 
     return 0;
 }
+
 
